dce366373941222b886bf83fc55adfe2e1fdfc59
Yuri Skripal <yjura@protonmail.com>
2026-05-25 12:04:05 +0300

    feat: add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 
 import './assets/shadcn-classless.css';
 import './assets/index.css';
 
+const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
 
 declare module '@tanstack/react-router' {
@@ -19,7 +21,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
