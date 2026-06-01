62b5e0f18702335be3a35c6418ed698665f5d8bf
Yuri Skrypal <yjura@protonmail.com>
2026-06-01 18:16:02 +0300

    fix: return error message

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -5,6 +5,7 @@ export const STALE_TIME = 15 * 60 * 1000;
 export const storageKey = 'query';
 const defaultTerm = 'terminator';
 const apiKey = getApiKey();
+export const errorMessage = 'An unexpected non-error exception occurred.';
 
 export const url = 'https://www.omdbapi.com/';
 export const getUrl = (searchTerm: string, page: number) =>
